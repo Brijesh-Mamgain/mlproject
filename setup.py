23d333748935f38ced33198767c6558be0a08943
Brijesh.Mamgain <flair99@gmail.com>
2023-09-18 08:03:29 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages ,setup
+from typing import List
+
+HYPEN_E_DOT="-e ."
+
+def get_requirement(file_path:str)->List[str]:
+    # function to return the list of requirements
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readline()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+
+setup(
+name="mlproject",
+version="0.0.1",
+author="Brijesh",
+author_email="flair99@gmail.com",
+packages=find_packages(),
+install_requires=get_requirement("requirements.txt")
+)
